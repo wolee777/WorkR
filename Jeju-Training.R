e1ddee12679d2c54507b02ee676f248637645815
wolee777 <wolee777@gmail.com>
2019-11-28 06:46:08 +0900

    제주 2일차 제어문 실습문제

---
+++ Jeju-Training.R
@@ -0,0 +1,25 @@
+#
+# 1 ~ 1000사이의 3의배수와 5의 배수를 한 줄에 10개씩 출력하고
+# 마지막에 개수를 출력
+#
+i <- 1
+count <- 0
+lineCount <- 1
+multiple3 <- NULL
+multiple5 <- NULL
+
+while ( i <= 1000 ) {
+  multiple3 <- i %% 3
+  multiple5 <- i %% 5
+  if ( multiple3 == 0 | multiple5 == 0 ) {
+    count <- count + 1
+    cat( i, " " )
+    lineCount <- lineCount + 1
+    if ( lineCount > 10 ) {
+      lineCount <- 1
+      print( '\n' )
+    }
+  }
+}
+
+cat( '\n', '1 ~ 1000사이의 3의 배수와 5의 배수의 개수 : ', count, '\n' )
